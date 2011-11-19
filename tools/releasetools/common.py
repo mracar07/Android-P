ed1b75c98c556cf59eb15f94427fce7eba6c5ad4
Brandon Bennett <bennetb@gmail.com>
2011-11-19 16:02:04 -0700

    Add ext2, ext3, and vfat to releasetools
    Support some non-standard partition that may be found in recovery.fstab
    
    Change-Id: I0026a1d3ae5d432db10150d1a15f7bc8e7a6054b
    Signed-off-by: mracar07 <mr.acar07@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1804,7 +1804,10 @@ PARTITION_TYPES = {
     "ext4": "EMMC",
     "emmc": "EMMC",
     "f2fs": "EMMC",
-    "squashfs": "EMMC"
+    "squashfs": "EMMC",
+    "ext2": "EMMC",
+    "ext3": "EMMC",
+    "vfat": "EMMC"
 }
 
 def GetTypeAndDevice(mount_point, info):
