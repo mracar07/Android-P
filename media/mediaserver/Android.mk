08ff9573a5c574f2665903a666201fbb965cb85c
Adrian DC <radian.dc@gmail.com>
2018-02-18 22:20:05 +0100

    Revert "WIP: Legacy fix to amend when merged"
    This reverts commit 158605606c428cbb98a4bcfba151601050b1c5a0.
    
    Signed-off-by: mracar07 <mr.acar07@gmail.com>

--- media/mediaserver/Android.mk
+++ media/mediaserver/Android.mk
@@ -46,8 +46,7 @@ ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1),true)
         libgui \
         libhidltransport \
         android.hardware.camera.common@1.0 \
-        android.hardware.camera.provider@2.4 \
-        vendor.qti.hardware.camera.device@1.0
+        android.hardware.camera.provider@2.4
 endif
 
 include $(BUILD_EXECUTABLE)
